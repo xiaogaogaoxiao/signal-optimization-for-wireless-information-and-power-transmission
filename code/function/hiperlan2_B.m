979acf8e0504cbeec89ea9ffdedbc155dc119002
SnowzTail <i@snowztail.com>
2019-06-28 16:09:45 +0100

    🚑 Denormalize tap power

--- code/function/hiperlan2_B.m
+++ code/function/hiperlan2_B.m
@@ -19,9 +19,6 @@ tapDelay = [0 10 20 30 50 80 110 140 180 230 280 330 380 430 490 560 640 730] * 
 tapPowerDb = [-2.6 -3.0 -3.5 -3.9 0.0 -1.3 -2.6 -3.9 -3.4 -5.6 -7.7 -9.9 -12.1 -14.3 -15.4 -18.4 -20.7 -24.6];
 tapPower = db2pow(tapPowerDb);
 
-% normalise path power
-tapPower = tapPower / sum(tapPower);
-
 % model taps as i.i.d. CSCG variables
 tapGain = sqrt(tapPower) .* (sqrt(1 / 2) * (randn(size(tapPower)) + 1i * randn(size(tapPower))));
 
